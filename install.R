712f4eb67e8062d6523f5e43255e52e0797d3f42
colin <colin@thinkr.fr>
2020-07-27 12:15:43 +0200

    the r-horde package is now installed on npm install

--- install.R
+++ install.R
@@ -1,9 +1,15 @@
+#!/usr/bin/env Rscript --vanilla
 (function(){
     if (!requireNamespace("remotes", quietly = TRUE)){
         dir.create("rtemplib")
         install.packages("remotes", lib = "rtemplib", repos = "https://cloud.r-project.org/")
         on.exit(unlink("rtemplib", TRUE))
     }
+    
     library(remotes, lib.loc = "rtemplib")
-    install_local("r-hordes")
+
+    install_github("colinfay/hordes", subdir = "r-hordes")
+    
+    install.packages("Rserve", repos = "https://cloud.r-project.org/")
+
 })()
